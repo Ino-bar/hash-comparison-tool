cb003e6eb148f1aa3a28a4e637d3badc2b8b0654
ino-bar <50051423+ino-bar@users.noreply.github.com>
2021-02-05 10:20:13 +0000

    05-02-21 fixed csv reader to get all columns

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -80,7 +80,7 @@ namespace hash_comparison_tool.Controllers
                 students.StudentList[i].FirstName = entries.ElementAt(2).ToString();
                 students.StudentList[i].CID = entries.ElementAt(entries.Length - 4).ToString();
 
-                for (int j = 5; j < entries.Length - 4; j += 6)
+                for (int j = 5; j < entries.Length - 3; j += 6)
                 {
                     QuestionSubmissions questionSubmission = new QuestionSubmissions();
                     questionSubmission.QuestionNumber = entries.ElementAt(j - 2).ToString();
